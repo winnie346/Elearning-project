bfd2fdb5a34d10cb84d9d908f863dbb790c758f2
winfred wambui <winniewambui159@gmail.com>
2021-07-08 14:09:44 +0300

    first commit

--- Moderator/pexams.php
+++ Moderator/pexams.php
@@ -5,14 +5,115 @@ include_once("../mheader.php");
 ?>
 
 
+<div class="app-main__outer">
+    <div class="app-main__inner">
+        <div class="app-page-title">
+            <div class="page-title-wrapper">
+                <div class="page-title-heading">
+                    <div>MANAGE EXAM</div>
+                </div><br>
+                <div class="row pt-2">
+                    <div class="col-lg-3 col-md-6 col-lg-12">
+                        <button class=" btn btn-primary" data-toggle="modal" data-target="#exampleModal" style="border: none; float:center;"><i class="fa fa-book"></i> &nbsp; create exam</button>
+                        <button class="course-dash btn btn-primary" data-toggle="modal" data-target="#exampleModal2" style="border: none; float:right;"> <i class="fa fa-bookmark"></i> &nbsp; Exam dashboard</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-12 pt-5">
+            <div class="main-card mb-3 card">
+
+
+
+                <div class="table-responsive">
+                    <table class="align-middle mb-0 table table-borderless table-striped table-hover" id="tableList">
+                        <thead>
+                            <tr>
+                                <th class="text-left pl-4">Exam Title</th>
+                                <th class="text-left ">Course</th>
+                                <th class="text-left ">Description</th>
+                                <th class="text-left ">Time limit</th>
+                                <th class="text-left ">Display limit</th>
+                                <th class="text-center" width="20%">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            </td>
+
+                            <td class="text-center">
+                                <a href="manage-exam.php?id=<?php echo $selExamRow['ex_id']; ?>" type="button" class="btn btn-primary btn-sm">Manage</a>
+                                <button type="button" id="deleteExam" data-id='<?php echo $selExamRow['ex_id']; ?>' class="btn btn-danger btn-sm">Delete</button>
+                            </td>
+                            </tr>
+
+
+
+                            <tr>
+                                <td colspan="5">
+                                    <h3 class="p-3">No Exam Found</h3>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+    <div class="pop-conainer">
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Add Exam</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form action="../Includes/lesson.php" method="post" enctype="multipart/form-data">
+                        <div class="modal-body">
+                            <div class="col-lg-12">
+                                <input type="hidden" name="title" class="form-control" placeholder="course">
+                                <input type="text" name="topic" class="form-control" placeholder="Exam title">
+                            </div><br>
+                            <div class="col-lg-12">
+                                <input type="text" name="title" class="form-control" placeholder="course">
+                            </div><br>
+                            <div class="col-lg-12">
+                                <input type="text" name="title" class="form-control" placeholder="Description">
+                            </div><br>
+                            <div class="col-lg-12">
+                                <input type="text" name="classroom" class="form-control" placeholder="Date time">
+                            </div><br>
+
+                            <div class="col-lg-12">
+                                <input type="text" name="notes" class="form-control" placeholder="Exam duration">
+                            </div><br>
+
+                            <div class="col-lg-12">
+                                <input type="url" name="url" class="form-control" placeholder="Action">
+                            </div><br>
+
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                <button type="submit" name="submit" class="btn btn-primary">Post</button>
+                            </div>
+                    </form>
+
+                </div>
+            </div>
 
 
 
 
+        </div>
 
-</div>
-
-</div>
+    </div>
 
 
 
